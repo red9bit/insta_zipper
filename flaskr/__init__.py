2dbba0a849006a47e13b30f241ba59641638b23e
Khashayar <khashayar.rajaee@gmail.com>
2020-07-25 00:24:48 +0430

    Remove `instance_relative_config=True` from app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,7 @@ from . import api
 
 def create_app():
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_pyfile('config.py', silent=True)
     app.register_blueprint(api.bp)
     return app
