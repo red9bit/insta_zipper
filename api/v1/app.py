5749835ea728023123a6e6405daf3523705f6660
Khashayar <khashayar.rajaee@gmail.com>
2020-07-24 19:55:52 +0430

    Improve `auth` API handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,22 @@
-from flask import Flask, request
-from .utils import create_auth_response
+from flask import Flask, request, render_template
+
+from utils.utils import create_auth_context, telegram_share_url_generator
 
 app = Flask(__name__)
 
 
-@app.route('/auth')
+@app.route('v1/auth')
 def auth():
     auth_code = request.args.get('code')
     if not auth_code:
-        return create_auth_response(False, '`code` query param does not exist'), 400
-    return create_auth_response(True, 'success', code=auth_code), 200
+        context = create_auth_context(False, 'Something went wrong!')
+        return render_template('instagram_callback.html', **context)
+
+    clean_auth_code = auth_code[:-2]  # remove #_ from the end
+    url = telegram_share_url_generator(clean_auth_code)
+    message = 'Great! Please click on the link below and then select InstaZipper bot to continue...'
+    context = create_auth_context(True, message, url=url)
+    return render_template('instagram_callback.html', **context)
 
 
 @app.route('/get_updates')
