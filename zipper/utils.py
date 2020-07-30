5acdae1b95a09c5d3ba3bd0b56ffc998424b36df
Khashayar <khashayar.rajaee@gmail.com>
2020-07-30 15:03:34 +0430

    Add a simple downloader

--- zipper/utils.py
+++ zipper/utils.py
@@ -1,3 +1,7 @@
+import requests
+import os
+
+
 def telegram_api_uri_generator(method_name):
     from flask import current_app
     config = current_app.config
@@ -5,3 +9,16 @@ def telegram_api_uri_generator(method_name):
         token=config['TELEGRAM_BOT_TOKEN'],
         method_name=method_name
     )
+
+
+def download_and_save(url, route, file_name):
+    path = f'{route}/{file_name}'
+    if not os.path.exists(path):
+        print('Download for url: %s started!' % url)
+        response = requests.get(url)
+        open(path, 'wb').write(response.content)
+        print('Download completed!')
+        return path
+    else:
+        print('%s is already exists!' % url)
+        return None
