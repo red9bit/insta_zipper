509e02dd107709a6c76a44fea077da33d0599407
Khashayar <khashayar.rajaee@gmail.com>
2020-07-30 13:10:24 +0430

    Add `instagram_auth_collection` and store user Instagram `auth_code` into MongoDB

--- flaskr/api.py
+++ flaskr/api.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template, request
+import datetime
+
+from flask import Blueprint, render_template, request, current_app as app
 from zipper.telegram_bot import send_message
+
+from utils.utils import connect_to_mongodb
 from .utils import create_auth_context, telegram_share_url_generator, instagram_auth_url_generator
 
 bp = Blueprint('api', __name__, url_prefix='/api')
@@ -13,6 +17,16 @@ def instagram_authenticate():
         url = telegram_share_url_generator(clean_auth_code)
         message = 'Great! Please click on the link below and then select InstaZipper bot to continue...'
         context = create_auth_context(True, message, url=url)
+
+        # write Instagram auth code into MongoDB
+        client, db = connect_to_mongodb()
+        instagram_auth_collection = db[app.config['COLLECTION_INSTAGRAM_AUTH_CODES']]
+        instagram_auth_collection.insert_one({
+            'date_stats': datetime.datetime.now(),
+            'source_ip': request.remote_addr,
+            'auth_code': clean_auth_code,
+        })
+        client.close()
     else:
         context = create_auth_context(False, 'Something went wrong!')
     return render_template('api/instagram_callback.html', **context)
