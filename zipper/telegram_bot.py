f97f25ae4e20a5b636053ba177f0803436e15a39
Khashayar <khashayar.rajaee@gmail.com>
2020-07-30 14:59:57 +0430

    Add telegram API `send_document` function

--- zipper/telegram_bot.py
+++ zipper/telegram_bot.py
@@ -17,3 +17,24 @@ def send_message(chat_id, text):
         print('Message has been sent to the user')
     else:
         print('Could not send the message to user! something went wrong!')
+
+
+def send_document(chat_id, file_path):
+    data = {
+        'chat_id': chat_id
+    }
+    files = {
+        'document': open(file_path, mode='rb')
+    }
+
+    ok, _ = request(
+        telegram_api_uri_generator('sendDocument'),
+        method='POST',
+        data=data,
+        files=files
+    )
+
+    if ok:
+        print('File has been sent to the user')
+    else:
+        print('Could not send file to user! something went wrong!')
